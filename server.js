af76721bc631633037ad158cc3ca375242d57990
Tratast <ryanbrooks@Ryans-MacBook-Pro.local>
2019-08-25 12:24:14 -0700

    We can grab Price stuff....eventually

--- server.js
+++ server.js
@@ -13,15 +13,16 @@ const createTableSQL = "\
                         icon TEXT\
                         )";
 const createPriceTableSQL = "\
-                             CREATE TABLE IF NOT EXISTS item(\
-                             id INTEGER,\
-                             date INTEGER,\
-                             daily INTEGER,\
-                             average INTEGER,\
-                             PRIMARY KEY (id, date)\
+                             CREATE TABLE IF NOT EXISTS price(\
+                             id INTEGER NOT NULL,\
+                             date INTEGER NOT NULL,\
+                             daily INTEGER NOT NULL,\
+                             average INTEGER NOT NULL,\
+                             PRIMARY KEY (id, date),\
+                             FOREIGN KEY (id) REFERENCES item(id)\
                              )";
 const insertItemSQL = "INSERT INTO item (id, name, description, type, icon) VALUES ";
-const insertPriceSQL = "INSERT INTO item (id, date, daily, average) VALUES ";
+const insertPriceSQL = "INSERT INTO price (id, date, daily, average) VALUES ";
 let app = express();
 
 let database = new sqlite3.Database('./items.db', (error) => {
@@ -32,7 +33,7 @@ let database = new sqlite3.Database('./items.db', (error) => {
     console.log('connected to database');
 });
 
-let priceEntry = new sqlite3.Database('./prices.db', (error) => {
+let priceEntry = new sqlite3.Database('./items.db', (error) => {
     if (error) {
         console.log("Dude, where's my database?");
     }
@@ -66,37 +67,82 @@ handlePageResponse = (page) => {
      }
 
      let dailyKeys = Object.keys(pricePage.daily);
+     //console.log(`Length of dailyKeys BEFORE:\t${dailyKeys.length}`);
+
+     //Run comparison of current data, return only new data
+     /*let newdailyKeys = function(callback) {
+         database.all(`SELECT date FROM price WHERE id = ?`, [id], (error, rows) => {
+            rows.forEach(row => {
+                //we juse need to compare the dates (row.date)
+                for (let index = 0; index < dailyKeys.length; index++) {
+                    console.log(`dailyKeys[${index}] = ${dailyKeys[index]}\t\trow.date = ${row.date}`);
+                    if (dailyKeys[index] == row.date) {
+                        console.log(`ATTEMPING TO REMOVE ${dailyKeys[index]} FROM INDEX ${index}`);
+                        dailyKeys = dailyKeys.splice(index, 1);
+                        break;
+                    }
+                }
+            });
+            callback(dailyKeys);
+        });
+     }
+    console.log(`Length of newdailyKeys = ${newdailyKeys.length}`,newdailyKeys);
+     console.log(`Length of dailyKeys AFTER:\t${dailyKeys.length}`);*/
+
+
      let timeStrings = dailyKeys.reduce((list, key) => {
-         list.push(`(${id}, ${parseInt(key)}, ${pricePage.daily[key]}, ${pricePage.average[key]})`);
-         return list;
-     }, []);
+        list.push(`(${id}, ${parseInt(key)}, ${pricePage.daily[key]}, ${pricePage.average[key]})`);
+        return list;
+    }, []);
      
      let insertStatment = insertPriceSQL + timeStrings.join(',');
-     //console.log(insertPriceSQL + timeStrings.join(','));
 
      priceEntry.run(insertStatment, (error) => {
          if (error) {
-             console.log("SQL ERROR" + error);
+             console.log("SQL ERROR:\t" + error);
          }
      });
 
  }
 
- getPrice = (idList, index) => {
-     fetch(graphApi + `${idList[index]}` + '.json').then(response => response.json()).then(result => {
-         handlePriceResponse(result, idList[index]);
-         getPrice(idList, index++);
-         return;
-     });
+ getPrice = async(idList, index, retry) => {
+     let apiResponse = await fetch(graphApi + `${idList[index]}` + '.json').then(response => response.json()).catch((error) => {
+        console.log(`HAD AN ISSUE GETTING PRICE INFORMATION FOR ITEM #${idList[index]}!\r${error}`);
+        console.log(`RESENDING PRICING QUERY FOR ${idList[index]}`);
+        retry = true;
+    });
+     
+    if (retry) {
+        getPrice(idList, index, false);
+        return;
+    }
+
+     await handlePriceResponse(apiResponse, idList[index]);
+     console.log(`Finished storing prices for item #${idList[index]}`);
+
+     if (idList.length - 1 > index) {
+        getPrice(idList, index + 1);
+     } else if (idList.length - 1 === index) {
+        console.log("DONE RETREIVING ALL OF THE PRICING INFORMATION!")
+     }
+     return;
  }
 
  getAllThePrices = () => {
-     //GRAB ID'S FROM ITEM DATABASE HERE
-     //let ids = ......
-     getPrice(ids, 0);
+     database.all(`SELECT id FROM item`, (error, rows) => {
+         let arr = [];
+
+         arr = rows.reduce((acc, row) => {
+             //console.log(row,row.id);
+             acc.push(row.id);
+             return acc;
+         }, []);
+
+         getPrice(arr, 0, false);
+     });
  }
 
- //getAllThePrices();
+ getAllThePrices();
 
 getPage = (category, letter, pageNum) => {
     fetch(categoriesApi + `category=${category}&alpha=${letter}&page=${pageNum}`).then((response) => response.json()).then((result) => {
@@ -130,8 +176,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/item', (req, res) => {
-    let itemSelect = `SELECT name, description, type, icon, id FROM item WHERE name = "${unescape(req.query.name)}" COLLATE NOCASE;`;
-    database.all(itemSelect, (error, rows) => {
+    let itemSelect = `SELECT name, description, type, icon, id FROM item WHERE name = ? COLLATE NOCASE;`;
+    database.all(itemSelect, [req.query.name], (error, rows) => {
         if(rows && rows.length > 0) {
             res.send(JSON.stringify(rows[0]));
         } else {
@@ -155,8 +201,8 @@ app.get('/allNames', (req, res) => {
 //Gotta grab that pricing information!
 //Should ask jztd about the purpose of the unescape() expression
 app.get('/price', (req, res) => {
-    let itemSelect = `SELECT id, date, daily, average FROM item WHERE id = ${unescape(req.query.id)};`;
-    priceEntry.all(itemSelect, (error, rows) => {
+    let itemSelect = `SELECT id, date, daily, average FROM item WHERE id = ?;`;
+    priceEntry.all(itemSelect, [req.query.id], (error, rows) => {
         res.send(JSON.stringify(rows));
     });
 });
